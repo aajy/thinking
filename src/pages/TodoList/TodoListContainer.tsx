66e537e94ef3101b0ee956e80ee707da27937864
aajy <aajy.000@gmail.com>
2024-06-18 17:44:54 +0900

    feat : addTask기능 함수 적용 UI구현
    - TodoListContainer에 custom hook 기능함수인 addTask 함수 적용
    - 피드백 : 더하는 기능만 가지는 컴포넌트로 분리
    
    Related to: #5

--- src/pages/TodoList/TodoListContainer.tsx
+++ src/pages/TodoList/TodoListContainer.tsx
@@ -1,3 +1,33 @@
+import { useState } from 'react';
+import { Input } from '../../components/Input';
+import '../../styles/todoList.scss';
+import useHandleLocalTodoList from '../../hooks/useHandleLocalTodoList';
+
 export const TodoListContainer = () => {
-	return <div>TodoList Container</div>;
+	const [currentTask, setCurrentTask] = useState<string>('');
+	const { addTask } = useHandleLocalTodoList();
+
+	const updateCurrentTask = (e: React.ChangeEvent<HTMLInputElement>) => {
+		if (!e.target.value.length) return;
+		setCurrentTask(e.target.value);
+	};
+
+	return (
+		<>
+			<h1>Todo List</h1>
+			<div className='todoListContainer'>
+				<Input onChange={updateCurrentTask} />
+
+				<button
+					onClick={() => {
+						if (addTask(currentTask)) {
+							setCurrentTask('');
+						}
+					}}
+				>
+					add
+				</button>
+			</div>
+		</>
+	);
 };
