3a860db03bd4f2e9bd5c67df3c2bdb642865407b
aajy <aajy.000@gmail.com>
2024-06-18 18:53:37 +0900

    fix: AddTask컴포넌트 props로 onClick실행함수 전달
    - 이벤트 함수 전달로 컴포넌트 추상화
    
    Related to: #5

---
+++ src/components/todoComponents/AddTask.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import { Input } from '../Input';
+
+interface AddTaskProps {
+	onClick: (task: string) => boolean;
+}
+
+export const AddTask = ({ onClick }: AddTaskProps) => {
+	const [currentTask, setCurrentTask] = useState<string>('');
+
+	const updateCurrentTask = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setCurrentTask(e.target.value);
+	};
+
+	return (
+		<>
+			<Input onChange={updateCurrentTask} />
+			<button
+				onClick={(e) => {
+					e.preventDefault();
+					if (onClick(currentTask)) {
+						setCurrentTask('');
+					} //else 는 'Failed to save to localStorage'가 찍히면서 초기화 안되도록.
+				}}
+			>
+				add
+			</button>
+		</>
+	);
+};
--- src/pages/TodoList/TodoListContainer.tsx
+++ src/pages/TodoList/TodoListContainer.tsx
@@ -1,32 +1,15 @@
-import { useState } from 'react';
-import { Input } from '../../components/Input';
 import '../../styles/todoList.scss';
 import useHandleLocalTodoList from '../../hooks/useHandleLocalTodoList';
+import { AddTask } from '../../components/todoComponents/AddTask';
 
 export const TodoListContainer = () => {
-	const [currentTask, setCurrentTask] = useState<string>('');
 	const { addTask } = useHandleLocalTodoList();
-
-	const updateCurrentTask = (e: React.ChangeEvent<HTMLInputElement>) => {
-		if (!e.target.value.length) return;
-		setCurrentTask(e.target.value);
-	};
 
 	return (
 		<>
 			<h1>Todo List</h1>
 			<div className='todoListContainer'>
-				<Input onChange={updateCurrentTask} />
-
-				<button
-					onClick={() => {
-						if (addTask(currentTask)) {
-							setCurrentTask('');
-						}
-					}}
-				>
-					add
-				</button>
+				<AddTask onClick={addTask} />
 			</div>
 		</>
 	);
